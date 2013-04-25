4df9dcae35ca2954a4f2c78148e578f2c58aa1ae
Larry Davis <lazdnet@gmail.com>
2013-04-24 18:31:24 -0700

    Some refactoring, better log output, and some missing semi-colons

--- server.js
+++ server.js
@@ -7,29 +7,26 @@ var MDNio = {
   searchHost: 'http://www.google.com/uds',
   defaultURL: 'https://developer.mozilla.org/en-US/docs/JavaScript',
   // Build search URL
-  getSearchURL: function(keyword) {
-    return MDNio.searchHost+'/GwebSearch?q='+encodeURIComponent(keyword+' site:developer.mozilla.org')+'&v=1.0';
+  getSearchURL: function(query) {
+    return MDNio.searchHost+'/GwebSearch?q='+encodeURIComponent(query+' site:developer.mozilla.org')+'&v=1.0';
   },
   // Get the MDN URL corresponding to a search
-  getURL: function(keyword, callback) {
-    console.log('Getting URL for:', keyword);
+  getURL: function(query, callback) {
+    console.log('Getting URL for:', query);
 
-    if (!keyword) return callback(null, MDNio.defaultURL);
-
-    var searchURL = MDNio.getSearchURL(keyword);
-    console.log('Searching for:', keyword);
+    // Return the default URL for empty queries
+    if (!query) return callback(null, MDNio.defaultURL);
 
     // Get results from Google
-    http.get(searchURL, MDNio.getResponse.bind(MDNio, function(error, result) {
-      var url = !error && result.responseData && result.responseData.results && result.responseData.results.length ? result.responseData.results[0].unescapedUrl : null;
-      console.log('Redirecting to:', url);
-      callback(error, url)
+    http.get(MDNio.getSearchURL(query), MDNio.getResponse.bind(MDNio, function(error, result) {
+      // On error or no result, redirect to default URL
+      var url = !error && result.responseData && result.responseData.results && result.responseData.results.length ? result.responseData.results[0].unescapedUrl : MDNio.defaultURL;
+      
+      callback(error, url);
     })).end();
   },
   // Handle the response from the server
   getResponse: function(callback, response) {
-    console.log('Response status:', response.statusCode);
-
     var str = '';
     response.on('data', function(chunk) {
       str += chunk;
@@ -46,18 +43,19 @@ var MDNio = {
   },
   // Handle requests from clients
   handleRequest: function (req, res) {
+    var query = req.url.slice(1);
     // Find the url corresponding to the passed path
-    MDNio.getURL(req.url.slice(1), function(error, url) {
-      // On error or empty URL, redirect to default URL
-      if (error || !url)
-          url = MDNio.defaultURL;
+    MDNio.getURL(query, function(error, url) {
+      if (error || !url) url = MDNio.defaultURL;
+
+      console.log(query+' => '+url);
 
       // Redirect to the URL
       res.writeHead(303, {'Location': url});
       res.end();    
     });
   }
-}
+};
 
 // Start a server
 http.createServer(MDNio.handleRequest).listen(port);
