0212e25991ca10ad1122fca7bce585c2caad67e7
Larry Davis <lazd@lazd.net>
2013-04-25 08:49:38 -0700

    Some restructuring, more configuration via environment variables, updated README

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # mdn.io
-> Short URLs for MDN
+> The "I'm feeling lucky" URL shortener
 
 ## Why?
 
-The [Mozilla Developer Network] is an awesome JavaScript resource. When looking for JavaScript references on something like `Function.apply`, you might Google "mdn apply" and click the first result.
+The [Mozilla Developer Network] is an awesome JavaScript resource. When looking for JavaScript references on something like `Function.apply`, you might Google "apply site:developer.mozilla.org" and click the first result.
 
-mdn.io does that for you :)
+mdn.io does that for you, and can be configured to [search any domain](#searching-other-domains).
 
 #### Use it in comments when explaining concepts
 
@@ -14,15 +14,13 @@ mdn.io does that for you :)
 expect(queue.add.length).to.equal(1);
 ```
 
-
 #### As a [lmgtfy] replacement for JavaScript questions
 
 > **friend:** dude, is it call() or apply() that takes an array?
 
 > **you:** mdn.io/apply
 
-
-#### As a quick way to look something up on the MDN
+#### Type directly in the address bar as a shortcut to search the MDN
 
 > [mdn.io/bind](http://mdn.io/bind)
 
@@ -38,11 +36,31 @@ mdn.io queries the [Google Web Search API] with `yourqueryhere site:developer.mo
 Note that, because this is a search, the page you're redirected to may change in the future. However, you can rest assured that you'll always be redirected to the page that Google finds most relevant.
 
 
+## Searching other domains
+
+mdn.io can be used to search any domain. See the [configuration](#configuration) section and fire up your own instance.
+
+
 ## Starting the server
 
 mdn.io has no dependencies, start it with:
 
 `PORT=8080 node server.js`
+
+### Configuration
+
+Configure mdn.io with the following environment variables.
+
+| Variable            | Description                                    | Default                                               |
+|:------------------- |:---------------------------------------------- |:----------------------------------------------------- |
+| **`PORT`**          | The port to run the server on.                 | `3000`                                                |
+| **`SEARCH_SERVER`** | The Google Web Search API server to use.       | `http://www.google.com/uds`                           |
+| **`SEARCH_DOMAIN`** | The domain to search.                          | `developer.mozilla.org`                               |
+| **`FALLBACK_URL`**  | The fallback URL for empty queries/no matches. | `https://developer.mozilla.org/en-US/docs/JavaScript` |
+
+#### Example: Reddit URL shortener
+
+`SEARCH_DOMAIN=reddit.com FALLBACK_URL=reddit.com PORT=8080 node server.js`
 
 
 [lmgtfy]: http://lmgtfy.com/?q=mdn%20apply
--- server.js
+++ server.js
@@ -1,32 +1,40 @@
 var http = require('http');
 
-// Get port from environment variables
-var port = process.env.PORT || 3000;
+/*
+  Environment variables:
+    PORT: The port to run the server on
+    SEARCH_SERVER: The Google Web Search API server to use
+    SEARCH_DOMAIN: The domain to search
+    FALLBACK_URL: The default URL for empty queries/no matches
+*/
 
-var MDNio = {
-  searchHost: 'http://www.google.com/uds',
-  defaultURL: 'https://developer.mozilla.org/en-US/docs/JavaScript',
+var mdnio = {
+  port: process.env.PORT || 3000,
+  searchServer: process.env.SEARCH_SERVER || 'http://www.google.com/uds',
+  searchDomain: process.env.SEARCH_DOMAIN || 'developer.mozilla.org',
+  fallbackURL: process.env.FALLBACK_URL || 'https://developer.mozilla.org/en-US/docs/JavaScript',
+
   // Build search URL
   getSearchURL: function(query) {
-    return MDNio.searchHost+'/GwebSearch?q='+encodeURIComponent(query+' site:developer.mozilla.org')+'&v=1.0';
+    return mdnio.searchServer+'/GwebSearch?q='+encodeURIComponent(query+' site:'+mdnio.searchDomain)+'&v=1.0';
   },
+
   // Get the MDN URL corresponding to a search
   getURL: function(query, callback) {
-    console.log('Getting URL for:', query);
-
     // Return the default URL for empty queries
-    if (!query) return callback(null, MDNio.defaultURL);
+    if (!query) return callback(null, mdnio.fallbackURL);
 
     // Get results from Google
-    http.get(MDNio.getSearchURL(query), MDNio.getResponse.bind(MDNio, function(error, result) {
+    http.get(mdnio.getSearchURL(query), mdnio.handlResponse.bind(mdnio, function(error, result) {
       // On error or no result, redirect to default URL
-      var url = !error && result.responseData && result.responseData.results && result.responseData.results.length ? result.responseData.results[0].unescapedUrl : MDNio.defaultURL;
+      var url = !error && result.responseData && result.responseData.results && result.responseData.results.length ? result.responseData.results[0].unescapedUrl : mdnio.fallbackURL;
       
       callback(error, url);
     })).end();
   },
+
   // Handle the response from the server
-  getResponse: function(callback, response) {
+  handlResponse: function(callback, response) {
     var str = '';
     response.on('data', function(chunk) {
       str += chunk;
@@ -41,28 +49,38 @@ var MDNio = {
       }
     });
   },
+
   // Decode the query
   getQuery: function(url) {
     return unescape(url.slice(1));
   },
+
   // Handle requests from clients
-  handleRequest: function (req, res) {
-    var query = MDNio.getQuery(req.url);
+  handleRequest: function(req, res) {
+    var query = mdnio.getQuery(req.url);
 
     // Find the url corresponding to the passed path
-    MDNio.getURL(query, function(error, url) {
-      if (error || !url) url = MDNio.defaultURL;
+    mdnio.getURL(query, function(error, url) {
+      if (error || !url) url = mdnio.fallbackURL;
 
       console.log(query+' => '+url);
 
       // Redirect to the URL
-      res.writeHead(303, {'Location': url});
-      res.end();    
+      res.writeHead(303, { 'Location': url });
+      res.end();
     });
+  },
+
+  startServer: function() {
+    // Start a server
+    http.createServer(mdnio.handleRequest).listen(mdnio.port);
+
+    console.log('mdn.io server running on port '+mdnio.port);
+    console.log('Search server: '+mdnio.searchServer);
+    console.log('Search domain: '+mdnio.searchDomain);
+    console.log('Default URL: '+mdnio.fallbackURL);
   }
 };
 
-// Start a server
-http.createServer(MDNio.handleRequest).listen(port);
-
-console.log('Server running on port '+port);
+// Start the server
+mdnio.startServer();
